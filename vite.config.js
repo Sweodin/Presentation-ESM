94202622a2466083f5c2881b9d312f3baab9e02c
Peter Gustafsson <petergustafsson910@gmail.com>
2025-12-01 09:46:13 +0100

    Add Netlify configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Presentation-ESM/',
+  base: process.env.NETLIFY ? '/' : '/Presentation-ESM/',
   server: {
     port: 3000,
     open: true
